2da743eb29ad680a2ddbfdfb55d93f0f94240d0f
Ibrahim Mammadov <zxyctn@gmail.com>
2024-12-05 14:25:02 +0100

    Add all details to run the server correctly

--- backend/start_server.sh
+++ backend/start_server.sh
@@ -1,4 +1,8 @@
 # Make sure to collect static files for API to have proper styles
-python manage.py collectstatic --noinput
+# python manage.py collectstatic --noinput
+
+# Start Redis server
+# redis-server
+
 # Starts Django server and serves API and websockets
 uvicorn backend.asgi:application --host localhost
