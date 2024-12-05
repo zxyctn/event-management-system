4b9a4ce79dde25690314826caa72860c3132230f
Ibrahim Mammadov <zxyctn@gmail.com>
2024-12-05 01:17:55 +0100

    Update serializers to attach more info

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,10 +1,24 @@
 from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 from .models import Event, EventJoiner
 
 
 class EventSerializer(serializers.ModelSerializer):
+    joiners = serializers.SerializerMethodField()
+    organizer = serializers.SerializerMethodField()
+
+    def get_joiners(self, obj):
+        return [
+            {"username": joiner.username, "id": joiner.id}
+            for joiner in obj.joiners.all()
+        ]
+
+    def get_organizer(self, obj):
+        return {"username": obj.organizer.username, "id": obj.organizer.id}
+
     class Meta:
         model = Event
         fields = "__all__"
@@ -17,6 +31,36 @@ class EventJoinerSerializer(serializers.ModelSerializer):
 
 
 class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True, validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+
+    password = serializers.CharField(
+        write_only=True, required=True, validators=[validate_password]
+    )
+    password2 = serializers.CharField(write_only=True, required=True)
+
     class Meta:
         model = User
-        fields = "__all__"
+        fields = ("id", "username", "email", "password", "password2")
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["password2"]:
+            raise serializers.ValidationError(
+                {"password": "Password fields didn't match."}
+            )
+
+        return attrs
+
+    def create(self, validated_data):
+        email = validated_data["email"]
+
+        if not email:
+            raise serializers.ValidationError({"email": "This field is required."})
+
+        user = User.objects.create(username=validated_data["username"], email=email)
+        password = validated_data.pop("password")
+        user.set_password(password)
+        user.save()
+
+        return user
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,16 +3,29 @@ from django.urls import include, path
 from rest_framework import routers
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from api.views import EventJoinerViewSet, EventViewSet
+from api.views import (
+    EventJoinerViewSet,
+    EventViewSet,
+    JoinEventView,
+    LeaveEventView,
+    LogoutView,
+    MeView,
+    SignUpView,
+)
 
 router = routers.DefaultRouter()
 router.register(r"events", EventViewSet)
 router.register(r"event-joiners", EventJoinerViewSet)
 
-urlpatterns = router.urls
-urlpatterns += [
+urlpatterns = [
+    path("api/", include(router.urls)),
     path("api-auth/", include("rest_framework.urls")),
     path("admin/", admin.site.urls),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("signup/", SignUpView.as_view(), name="auth_signup"),
+    path("me/", MeView.as_view(), name="auth_me"),
+    path("logout/", LogoutView.as_view(), name="auth_logout"),
+    path("api/events/<int:id>/join/", JoinEventView.as_view(), name="join_event"),
+    path("api/events/<int:id>/leave/", LeaveEventView.as_view(), name="leave_event"),
 ]
